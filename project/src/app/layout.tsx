26193dd1197c05859b6fa23cc110fa8ee00ca6d7
Ahmed Haddaji <137641951+HaddajiDev@users.noreply.github.com>
2024-06-26 08:46:21 +0100

    stuff

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import NavBar from "@/components/Navbar/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <div><NavBar /></div>
+        <div className="marg">{children}</div>
+        </body>
     </html>
   );
 }
--- project/src/app/page.tsx
+++ project/src/app/page.tsx
@@ -1,8 +1,21 @@
-import Image from "next/image";
+"use client";
+
+import React, { useState, useEffect } from 'react';
+
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
+  const controls = useAnimation();
+  useEffect(() => {
+      controls.start({ opacity: 1 });
+  }, [controls]);
+  
   return (
     <div>
+      <motion.div>
+        
+      </motion.div>   
+      <h1>Ahmed</h1>
       <h1>Ahmed</h1>
     </div>
   );
