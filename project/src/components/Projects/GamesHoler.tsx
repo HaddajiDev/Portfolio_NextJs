ececf474b56e06b0d14d7517b758e02659ed4828
Ahmed Haddaji <ahmedhaddajiahmed@gmail.com>
2024-06-27 23:05:52 +0100

    last

--- project/src/components/Projects/GamesHoler.tsx
+++ project/src/components/Projects/GamesHoler.tsx
@@ -1,125 +1,85 @@
 import React, { useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import {AllGames, AllGames_2, AllGames_3, AllGames_4, AllGames_5, AllProjects} from './Project';
+import { AllGames, AllGames_2, AllGames_3, AllGames_4, AllGames_5 } from './Project';
 import Image from 'next/image';
 import arrow from '@/app/imgs/Arrow.png';
 
-function GamesHoler({el}:any) {
+const useScrollAnimation = (id: any) => {
     const controls = useAnimation();
+
     useEffect(() => {
         const handleScroll = () => {
-            const element = document.getElementById(el[0].name);
+            const element = document.getElementById(id);
             if (element) {
                 const { top } = element.getBoundingClientRect();
                 const isInView = top >= 0 && top <= window.innerHeight;
                 if (isInView) {
                     controls.start({ x: 0, opacity: 1 });
                 } else {
-                    controls.start({ x: -20, opacity: 0 });
+                    controls.start({ x: -50, opacity: 0 });
                 }
             }
-        };       
-        handleScroll();        
+        };
+
+        handleScroll();
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
+    }, [controls, id]);
 
-    }, [controls]);
+    return controls;
+};
 
-  return (
-    <div className='container-fluid'>
-        <div className='row'>
-            <div className='project-holder'>
-                {AllGames.map((el) => 
-                    <motion.div   
-                        id={el.name}
-                        initial={{x: -50, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        animate={controls}
-                        transition={{duration: 0.5}}                     
-                        >
-                        <div className='project-img col-sm-12 col-md-12'>
-                            <Image src={el.img} alt='' width={el.width} height={el.height} className='img-fluid hover-target'/>
-                            <h2 className='project-name'>{el.name}</h2>
-                            <a style={{all: 'unset'}} href={el.url} target='_blank'><Image src={arrow} alt='' width={50} className='arrow'/></a>
-                        </div>
-                    </motion.div>
-                )}
-            </div>
-        </div>        
-        <div className='row'>
-            <div className='project-holder'>
-                {AllGames_2.map((el) =>
-                    <motion.div   
-                        id={el.name}
-                        initial={{x: -50, opacity: 0 }}
-                        whileInView={{ x: 0, opacity: 1 }}
-                        animate={controls}
-                        transition={{duration: 0.5}}                     
-                    >
-                        <div className='project-img'>
-                            <Image src={el.img} alt='' width={el.width} height={el.height} className='img-fluid hover-target'/>
-                            <h2 className='project-name'>{el.name}</h2>
-                            <a style={{all: 'unset'}} href={el.url} target='_blank'><Image src={arrow} alt='' width={50} className='arrow'/></a>
-                        </div>
-                    </motion.div>
-                )}
-            </div>
-        </div>
-        <div className='project-holder'>
-            {AllGames_3.map((el) => 
+function GamesHoler() {
+    const renderProjects = (projects: any) => {
+        return projects.map((el: any) => {
+            const controls = useScrollAnimation(el.name);
+
+            return (
                 <motion.div
+                    key={el.name}
                     id={el.name}
-                    initial={{x: -50, opacity: 0 }}
+                    initial={{ x: -50, opacity: 0 }}
                     whileInView={{ x: 0, opacity: 1 }}
                     animate={controls}
-                    transition={{duration: 0.5}}                     
+                    transition={{ duration: 0.5 }}
                 >
-                    <div className='project-img'>
-                        <Image src={el.img} alt='' width={el.width} height={el.height} className='img-fluid hover-target'/>
+                    <div className='project-img col-sm-12 col-md-12'>
+                        <Image src={el.img} alt={el.name} width={el.width} height={el.height} className='img-fluid hover-target' />
                         <h2 className='project-name'>{el.name}</h2>
-                        <a style={{all: 'unset'}} href={el.url} target='_blank'><Image src={arrow} alt='' width={50} className='arrow'/></a>
+                        <a style={{ all: 'unset' }} href={el.url} target='_blank'>
+                            <Image src={arrow} alt='arrow' width={50} className='arrow' />
+                        </a>
                     </div>
                 </motion.div>
-            )}
+            );
+        });
+    };
+
+    return (
+        <div className='container-fluid'>
+            <div className='row'>
+                <div className='project-holder'>
+                    {renderProjects(AllGames)}
+                </div>
+            </div>
+            <div className='row'>
+                <div className='project-holder'>
+                    {renderProjects(AllGames_2)}
+                </div>
+            </div>
+            <div className='project-holder'>
+                {renderProjects(AllGames_3)}
+            </div>
+            <div className='project-holder'>
+                {renderProjects(AllGames_4)}
+            </div>
+            <div className='project-holder'>
+                {renderProjects(AllGames_5)}
+            </div>
         </div>
-        <div className='project-holder'>
-            {AllGames_4.map((el) => 
-                <motion.div   
-                    id={el.name}
-                    initial={{x: -50, opacity: 0 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                    animate={controls}
-                    transition={{duration: 0.5}}                     
-                >
-                    <div className='project-img'>
-                        <Image src={el.img} alt='' width={el.width} height={el.height} className='img-fluid hover-target'/>
-                        <h2 className='project-name'>{el.name}</h2>
-                        <a style={{all: 'unset'}} href={el.url} target='_blank'><Image src={arrow} alt='' width={50} className='arrow'/></a>
-                    </div>
-                </motion.div>
-            )}
-        </div>
-        <div className='project-holder'>
-            {AllGames_5.map((el) => 
-                <motion.div   
-                    id={el.name}
-                    initial={{x: -50, opacity: 0 }}
-                    whileInView={{ x: 0, opacity: 1 }}
-                    animate={controls}
-                    transition={{duration: 0.5}}                     
-                >
-                    <div className='project-img'>
-                        <Image src={el.img} alt='' width={el.width} height={el.height} className='img-fluid hover-target'/>
-                        <h2 className='project-name'>{el.name}</h2>
-                        <a style={{all: 'unset'}} href={el.url} target='_blank'><Image src={arrow} alt='' width={50} className='arrow'/></a>
-                    </div>
-                </motion.div>
-            )}
-        </div>
-    </div>
-  )
+    );
 }
 
-export default GamesHoler
+export default GamesHoler;
--- project/src/components/Projects/ProjectCard.tsx
+++ project/src/components/Projects/ProjectCard.tsx
@@ -1,73 +1,86 @@
-import React, { useEffect, useState } from 'react';
-import { motion, useAnimation } from 'framer-motion';
-import {AllProjects} from './Project';
-import Image from 'next/image';
-import arrow from '@/app/imgs/Arrow.png';
-import arrow_black from '@/app/imgs/Arrow_black.png';
+import { useEffect } from 'react';
+import { useAnimation } from 'framer-motion';
 
-function ProjectCard({el}: any) {    
+const useScrollAnimation = (id: any) => {
     const controls = useAnimation();
+
     useEffect(() => {
         const handleScroll = () => {
-            const element = document.getElementById(el[0].name);
+            const element = document.getElementById(id);
             if (element) {
                 const { top } = element.getBoundingClientRect();
                 const isInView = top >= 0 && top <= window.innerHeight;
                 if (isInView) {
                     controls.start({ x: 0, opacity: 1 });
                 } else {
-                    controls.start({ x: -20, opacity: 0 });
+                    controls.start({ x: -50, opacity: 0 });
                 }
             }
-        };       
-        handleScroll();        
+        };
+
+        handleScroll();
         window.addEventListener('scroll', handleScroll);
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
+    }, [controls, id]);
 
-    }, [controls]); 
-   
-  return (
-    <>
-    <div className='container-fluid'>        
-        <div className='project-holder'>
-            {AllProjects.filter((el) => el.name != "E-commerce app").map((el) =>
+    return controls;
+};
+import React from 'react';
+import { motion } from 'framer-motion';
+import { AllProjects } from './Project';
+import Image from 'next/image';
+import arrow from '@/app/imgs/Arrow.png';
+import arrow_black from '@/app/imgs/Arrow_black.png';
+
+
+function ProjectCard() {    
+    const renderProjects = (projects: any) => {
+        return projects.map((el: any) => {
+            const controls = useScrollAnimation(el.name);
+
+            return (
                 <motion.div
+                    key={el.name}
                     id={el.name}
-                    initial={{x: -50, opacity: 0 }}
-                    whileInView={{ x: 0, opacity: 1 }}
+                    initial={{ x: -50, opacity: 0 }}
                     animate={controls}
-                    transition={{duration: 0.5}}
+                    transition={{ duration: 0.5 }}
                 >
                     <div className='project-img'>
-                        <Image src={el.img} alt='' style={{height: el.height, width: el.width}} className='img-fluid hover-target'/>
+                        <Image src={el.img} alt='' style={{ height: el.height, width: el.width }} className='img-fluid hover-target' />
                         <h2 className='project-name'>{el.name}</h2>
-                        <Image src={arrow} alt='' width={50} className='arrow'/>
+                        <Image src={arrow} alt='' width={50} className='arrow' />
                     </div>
                 </motion.div>
-            )}            
-        </div>
-        <div className='project-holder'>
-            <motion.div
-                id='_project'
-                initial={{x: -50, opacity: 0 }}
-                whileInView={{ x: 0, opacity: 1 }}
-                animate={controls}
-                transition={{duration: 0.5}}
-                    >
+            );
+        });
+    };
+
+    const controlsForSpecialProject = useScrollAnimation('_project');
+
+    return (
+        <div className='container-fluid'>
+            <div className='project-holder'>
+                {renderProjects(AllProjects.filter((el) => el.name !== "E-commerce app"))}
+            </div>
+            <div className='project-holder'>
+                <motion.div
+                    id='_project'
+                    initial={{ x: -50, opacity: 0 }}
+                    animate={controlsForSpecialProject}
+                    transition={{ duration: 0.5 }}
+                >
                     <div className='project-img'>
-                        <Image src={AllProjects[2].img} alt='' style={{height: AllProjects[2].height, width: AllProjects[2].width}} className='img-fluid hover-target'/>
+                        <Image src={AllProjects[2].img} alt='' style={{ height: AllProjects[2].height, width: AllProjects[2].width }} className='img-fluid hover-target' />
                         <h2 className='project-name black'>{AllProjects[2].name}</h2>
-                        <Image src={arrow_black} alt='' width={50} className='arrow hover-target'/>
+                        <Image src={arrow_black} alt='' width={50} className='arrow hover-target' />
                     </div>
-            </motion.div>
+                </motion.div>
+            </div>
         </div>
-    </div>
-                
-        
-    </>
-  )
+    );
 }
 
-export default ProjectCard
+export default ProjectCard;
