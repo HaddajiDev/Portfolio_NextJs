8a0f38ee1339e5efebc106cccf31a2c6ef20bcd3
Ahmed Haddaji <ahmedhaddajiahmed@gmail.com>
2024-06-28 00:06:09 +0100

    stuff

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Analytics } from '@vercel/analytics/react';
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="" />
+      </head>
       <body className={inter.className}>      
-        <div className="marg">{children}</div>
+        <div className="marg">{children} <Analytics /></div>
         </body>
     </html>
   );
