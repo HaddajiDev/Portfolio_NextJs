f8d356657508ad7c6c659f7142e3327d95abb94d
Ahmed Haddaji <ahmedhaddajiahmed@gmail.com>
2024-08-16 09:14:27 +0100

    update

--- project/src/app/layout.tsx
+++ project/src/app/layout.tsx
@@ -3,12 +3,13 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Analytics } from '@vercel/analytics/react';
+import { url } from "inspector";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "HaddajiDev",
-  description: "Ahmed Haddaji protfolio",
+  description: "Ahmed Haddaji protfolio",  
 };
 
 export default function RootLayout({
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="" />
+        <link rel="icon" href="https://res.cloudinary.com/dqkvgv7mh/image/upload/v1723796041/zkhg2eb84wb8tmxvjay7.png" />
       </head>
       <body className={inter.className}>      
         <div className="marg">{children} <Analytics /></div>
