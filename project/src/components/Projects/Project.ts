56f76be6200221f25436fe30ec8e60a5a3b2b45e
HaddajiDev <ahmedhaddajiahmed@gmail.com>
2025-01-26 13:37:51 +0100

    update

--- project/src/components/Projects/Project.ts
+++ project/src/components/Projects/Project.ts
@@ -25,7 +25,47 @@ import pirate from '@/app/imgs/PirateDuel.png';
 
 import files from '@/app/imgs/files.png';
 
+import ai from '@/app/imgs/ai.png';
 
+
+class project {
+    name: string;
+    img: StaticImageData;
+    url: string;
+    width: number;
+    height: number;
+    row: number;
+    arrowColor: 'black' | 'white';
+
+    constructor(
+        name: string, 
+        img: StaticImageData, 
+        url: string, 
+        width: number, 
+        height: number,
+        row: number,
+        arrowColor: 'black' | 'white' = 'white'
+    ) {
+        this.name = name;
+        this.img = img;
+        this.url = url;
+        this.width = width;
+        this.height = height;
+        this.row = row;
+        this.arrowColor = arrowColor;
+    }
+}
+
+const project_Agence: project = new project("Agency app", agence, "https://agencyahmed.vercel.app/", 600, 300, 1, "white");
+const project_weather: project = new project("Weather app", weather, "https://weather-app-five-ruby-57.vercel.app/", 600, 300, 1, "black");
+const project_store: project = new project("E-commerce app", store, "https://ecommerce-front-pi-blue.vercel.app/", 600, 300, 2, "black");
+const project_blog: project = new project("Savage Blog", blog, "https://savageblog.vercel.app/", 600, 300, 2, "black");
+const project_files: project = new project("Savage Files", files, "https://savage-files.vercel.app", 600, 300, 3, "black");
+const project_savageai: project = new project("Savage AI", ai, "https://savageai-front.vercel.app/", 600, 300, 3, "white");
+
+const AllProjects : Array<project> = [project_Agence,project_weather, project_store, project_blog, project_files, project_savageai];
+
+// Games
 interface projects{
     name: string,
     img: StaticImageData,
@@ -34,7 +74,7 @@ interface projects{
     height: number
 }
 
-class project implements projects{
+class Gameproject implements projects{
     name: string;
     img: StaticImageData;
     url: string;
@@ -49,39 +89,31 @@ class project implements projects{
 	}
 }
 
-const project_Agence: project = new project("Agency app", agence, "https://agencyahmed.vercel.app/", 600, 300);
-const project_weather: project = new project("Weather app", weather, "https://weather-app-five-ruby-57.vercel.app/", 600, 300);
-const project_store: project = new project("E-commerce app", store, "https://ecommerce-front-pi-blue.vercel.app/", 600, 300);
-const project_blog: project = new project("Savage Blog", blog, "https://savageblog.vercel.app/", 600, 300);
-const project_files: project = new project("Savage Files", files, "https://savage-files.vercel.app", 600, 300);
-
-const AllProjects : Array<project> = [project_Agence,project_weather, project_store, project_blog, project_files];
-
-const project_gems: project = new project("Gems Rush", gems, "https://play.google.com/store/apps/details?id=com.haddajiDev.pirategemrush", 250, 450);
-const project_sliding: project = new project("Sliding puzzle", slding, "https://play.google.com/store/apps/details?id=com.haddajidev.slidingpuzzle", 250, 450);
-const project_memory: project = new project("Memory puzzle", memory, "https://haddajidev.itch.io/puzzle-pirates-memory-edition", 250, 450);
+const project_gems: Gameproject = new Gameproject("Gems Rush", gems, "https://play.google.com/store/apps/details?id=com.haddajiDev.pirategemrush", 250, 450);
+const project_sliding: Gameproject = new Gameproject("Sliding puzzle", slding, "https://play.google.com/store/apps/details?id=com.haddajidev.slidingpuzzle", 250, 450);
+const project_memory: Gameproject = new Gameproject("Memory puzzle", memory, "https://haddajidev.itch.io/puzzle-pirates-memory-edition", 250, 450);
 
 
-const AllGames: Array<project> = [project_gems, project_sliding, project_memory];
+const AllGames: Array<Gameproject> = [project_gems, project_sliding, project_memory];
 
-const project_chess: project = new project("Chess", chess, "https://haddajidev.itch.io/chess-with-no-checkmate", 600, 300);
-const AllGames_2: Array<project> = [project_chess];
+const project_chess: Gameproject = new Gameproject("Chess", chess, "https://haddajidev.itch.io/chess-with-no-checkmate", 600, 300);
+const AllGames_2: Array<Gameproject> = [project_chess];
 
-const project_clicker: project = new project("Treasure Clicker", clicker, "https://haddajidev.itch.io/treasure-clicker", 600, 300);
-const project_dog: project = new project("Dog Rescue", dog, "https://haddajidev.itch.io/dog-rescue", 600, 300);
-const AllGames_3: Array<project> = [project_clicker, project_dog];
+const project_clicker: Gameproject = new Gameproject("Treasure Clicker", clicker, "https://haddajidev.itch.io/treasure-clicker", 600, 300);
+const project_dog: Gameproject = new Gameproject("Dog Rescue", dog, "https://haddajidev.itch.io/dog-rescue", 600, 300);
+const AllGames_3: Array<Gameproject> = [project_clicker, project_dog];
 
-const project_wizard: project = new project("The Wizard and the...", wizard, "https://haddajidev.itch.io/the-wizard-and-the-warrior-time-keeper", 500, 250);
-const project_cave: project = new project("The Cursed Cave", cave, "https://haddajidev.itch.io/the-cursed-cave", 500, 250);
-const project_clicker_2: project = new project("رحلة النمو", clicker_2, "https://haddajidev.itch.io/gamejam", 500, 250);
-const AllGames_4: Array<project> = [project_wizard, project_cave, project_clicker_2];
+const project_wizard: Gameproject = new Gameproject("The Wizard and the...", wizard, "https://haddajidev.itch.io/the-wizard-and-the-warrior-time-keeper", 500, 250);
+const project_cave: Gameproject = new Gameproject("The Cursed Cave", cave, "https://haddajidev.itch.io/the-cursed-cave", 500, 250);
+const project_clicker_2: Gameproject = new Gameproject("رحلة النمو", clicker_2, "https://haddajidev.itch.io/gamejam", 500, 250);
+const AllGames_4: Array<Gameproject> = [project_wizard, project_cave, project_clicker_2];
 
-const project_bomb: project = new project("Bomb Skipper", bomb, "https://haddajidev.itch.io/bomb-skipper", 600, 300);
-const project_tank: project = new project("Tank Showdown", tank, "https://haddajidev.itch.io/tank-showdown", 600, 300);
-const AllGames_5 : Array<project> = [project_bomb, project_tank];
+const project_bomb: Gameproject = new Gameproject("Bomb Skipper", bomb, "https://haddajidev.itch.io/bomb-skipper", 600, 300);
+const project_tank: Gameproject = new Gameproject("Tank Showdown", tank, "https://haddajidev.itch.io/tank-showdown", 600, 300);
+const AllGames_5 : Array<Gameproject> = [project_bomb, project_tank];
 
 
-const merge_Ball: project = new project("Balls Merge", merge, "https://haddajidev.itch.io/balls-merge", 600, 300);
-const Pirate_Duel: project = new project("Pirate's Dual", pirate, "https://haddajidev.itch.io/pirates-dual", 600, 300);
-const AllGames_6 : Array<project> = [merge_Ball, Pirate_Duel];
+const merge_Ball: Gameproject = new Gameproject("Balls Merge", merge, "https://haddajidev.itch.io/balls-merge", 600, 300);
+const Pirate_Duel: Gameproject = new Gameproject("Pirate's Dual", pirate, "https://haddajidev.itch.io/pirates-dual", 600, 300);
+const AllGames_6 : Array<Gameproject> = [merge_Ball, Pirate_Duel];
 export {AllProjects, AllGames, AllGames_2, AllGames_3, AllGames_4, AllGames_5, AllGames_6};
--- project/src/components/Projects/ProjectCard.tsx
+++ project/src/components/Projects/ProjectCard.tsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import { AllProjects } from './Project';
 import Image from 'next/image';
 import arrow from '@/app/imgs/Arrow.png';
 import arrow_black from '@/app/imgs/Arrow_black.png';
+import { AllProjects } from './Project';
 
-const useScrollAnimation = (id: any) => {
+const useScrollAnimation = (id: string) => {
     const controls = useAnimation();
-    const [hasAnimated, setHasAnimated] = useState(false);
+    const [hasAnimated, setHasAnimated] = React.useState(false);
 
-    useEffect(() => {
+    React.useEffect(() => {
         const handleScroll = () => {
             const element = document.getElementById(id);
             if (element) {
@@ -24,15 +24,13 @@ const useScrollAnimation = (id: any) => {
 
         handleScroll();
         window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
+        return () => window.removeEventListener('scroll', handleScroll);
     }, [controls, hasAnimated, id]);
 
     return controls;
 };
 
-const Project = ({ project, arrowImg }: any) => {
+const ProjectComponent = ({ project }: { project: typeof AllProjects[number] }) => {
     const controls = useScrollAnimation(project.name);
 
     return (
@@ -43,77 +41,52 @@ const Project = ({ project, arrowImg }: any) => {
             transition={{ duration: 0.5 }}
         >
             <div className='project-img col-sm-12 col-md-12'>
-                <a style={{ all: 'unset' }} href={project.url} target='_blank'><Image src={project.img} alt='' style={{ height: project.height, width: project.width }} className='img-fluid hover-target' /></a>
-                {project.name == "E-commerce app" || project.name == "Savage Blog"|| project.name == "Savage Files"
-                    ?<h2 className='project-name black'>{project.name}</h2>
-                    :<h2 className='project-name'>{project.name}</h2>
-                }
-                {project.name == "E-commerce app" || project.name == "Savage Blog" || project.name == "Savage Files"
-                ?   <a style={{ all: 'unset' }} href={project.url} target='_blank'><Image src={arrow_black} alt='' width={50} className='arrow' /></a>
-                :   <a style={{ all: 'unset' }} href={project.url} target='_blank'><Image src={arrowImg} alt='' width={50} className='arrow' /></a>
-                }
+                <a style={{ all: 'unset' }} href={project.url} target='_blank' rel="noopener noreferrer">
+                    <Image 
+                        src={project.img} 
+                        alt={project.name} 
+                        style={{ height: project.height, width: project.width }}
+                        className='img-fluid hover-target'
+                    />
+                </a>
+                <h2 className={`project-name ${project.arrowColor === 'black' ? 'black' : ''}`}>
+                    {project.name}
+                </h2>
+                <a style={{ all: 'unset' }} href={project.url} target='_blank' rel="noopener noreferrer">
+                    <Image 
+                        src={project.arrowColor === 'black' ? arrow_black : arrow} 
+                        alt="Arrow icon" 
+                        width={50}
+                        className='arrow' 
+                    />
+                </a>
             </div>
         </motion.div>
     );
 };
 
 const ProjectCard = () => {
-    const controlsForSpecialProject = useScrollAnimation('_project');
+    const groupedProjects = AllProjects.reduce((acc, project) => {
+        const row = project.row.toString();
+        acc[row] = acc[row] || [];
+        acc[row].push(project);
+        return acc;
+    }, {} as Record<string, typeof AllProjects>);
 
     return (
         <div className='container-fluid'>
-            <div className='row'>
-                <div className='project-holder'>
-                    {AllProjects.filter((el) => el.name !== "E-commerce app" && el.name !== "Savage Blog" && el.name !== "Savage Files").map((el) => (
-                        <Project key={el.name} project={el} arrowImg={arrow} />
-                    ))}
+            {Object.entries(groupedProjects).map(([rowNumber, projects]) => (
+                <div className='row' key={`row-${rowNumber}`}>
+                    <div className='project-holder'>
+                        {projects.map((project) => (
+                            <ProjectComponent 
+                                key={project.name} 
+                                project={project} 
+                            />
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div className='row'>
-                <div className='project-holder'>
-                    {AllProjects.filter((el) => el.name !== "Agency app" && el.name !== "Weather app"  && el.name !== "Savage Files").map((el) => (
-                        <Project key={el.name} project={el} arrowImg={arrow} />
-                    ))}
-                </div>
-            </div>
-            <div className='row'>
-                <div className='project-holder'>
-                    {AllProjects.filter((el) => el.name !== "Agency app" && el.name !== "Weather app" && el.name !== "E-commerce app" && el.name !== "Savage Blog").map((el) => (
-                        <Project key={el.name} project={el} arrowImg={arrow} />
-                    ))}
-                </div>
-            </div>
-            {/* <div style={{display: 'flex'}}>
-                <div className='project-holder'>
-                    <motion.div
-                        id='_project'
-                        initial={{ x: -50, opacity: 0 }}
-                        animate={controlsForSpecialProject}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <div className='project-img'>
-                            <a style={{ all: 'unset' }} href={AllProjects[2].url} target='_blank'><Image src={AllProjects[2].img} alt='' style={{ height: AllProjects[2].height, width: AllProjects[2].width }} className='img-fluid hover-target' /></a>
-                            <h2 className='project-name black'>{AllProjects[2].name}</h2>
-                            <a style={{ all: 'unset' }} href={AllProjects[2].url} target='_blank'><Image src={arrow_black} alt='' width={50} className='arrow' /></a>
-                        </div>
-                    </motion.div>
-                </div>
-                <div className='project-holder'>
-                    <motion.div
-                        id='_project'
-                        initial={{ x: -50, opacity: 0 }}
-                        animate={controlsForSpecialProject}
-                        transition={{ duration: 0.5 }}
-                    >
-                        <div className='project-img'>
-                            <a style={{ all: 'unset' }} href={AllProjects[3].url} target='_blank'><Image src={AllProjects[3].img} alt='' style={{ height: AllProjects[3].height, width: AllProjects[3].width }} className='img-fluid hover-target' /></a>
-                            <h2 className='project-name black'>{AllProjects[3].name}</h2>
-                            <a style={{ all: 'unset' }} href={AllProjects[3].url} target='_blank'><Image src={arrow_black} alt='' width={50} className='arrow' /></a>
-                        </div>
-                    </motion.div>
-                </div>
-            </div> */}
-            
+            ))}
         </div>
     );
 };
